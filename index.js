7f28275d66fd85bccc18888dff3cd7a67102457c
Gabriel Nobrega <gnobrega.contato@gmail.com>
2019-11-26 09:05:43 -0300

    update

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const concurrently = require("concurrently");
+// const concurrently = require("concurrently");
 
-function success(msg){
-    console.log(msg);
-}
+// function success(msg){
+//     console.log(msg);
+// }
 
-function failure(msg) {
-  console.error(msg);
-}
+// function failure(msg) {
+//   console.error(msg);
+// }
 
-concurrently([{ command: "node packages/vtex-proxy/index", name: "vtex-proxy" }], {
-  prefix: "name",
-  killOthers: ["failure", "success"],
-  restartTries: 3
-}).then(success, failure);
+// concurrently([{ command: "node packages/vtex-proxy/index", name: "vtex-proxy" }], {
+//   prefix: "name",
+//   killOthers: ["failure", "success"],
+//   restartTries: 3
+// }).then(success, failure);
+
+const { fork } = require("child_process");
+
+const proxy = fork("@vtexy/vtex-proxy");
+
+console.log(proxy);
